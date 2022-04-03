5936e2ffb8fb3e65021b53ada5c3ca0c4c37080e
kendinitekmeleyenkedi <69451768+kendinitekmeleyenkedi@users.noreply.github.com>
2022-04-03 21:24:32 +0300

    Create arya.sh

---
+++ arya_data/arya.sh
@@ -0,0 +1,5 @@
+cat /opt/pharus/arya_data/bootstrap.min.css > /opt/pharus/html/css/bootstrap.min.css
+cat /opt/pharus/arya_data/index.blade.php > /opt/pharus/resources/views/about/index.blade.php
+cat /opt/pharus/arya_data/light.css > /opt/pharus/html/css/light.css
+cat /opt/pharus/arya_data/styles.css > /opt/pharus/html/css/styles.css
+cp /opt/pharus/arya_data/pharus_logo_menu.png /opt/pharus/html/images/custom/pharus_logo_menu.png
